4c363d603e5f4851f8003c99b14da3f373e94b14
Oleg Mihailik <mihailik@gmail.com>
2025-09-20 12:04:26 +0100

    Added bookmarklet and moved index.html out of stupid dist.

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,19 @@
-alert("hello from bookmarklet " + new Date())
+(async () => {
+  const uaih = 'https://uaih.london/';
+  const popup = window.open(uaih, '_blank', 'width=600,height=600');
+  await new Promise((resolve) => {
+    popup.onload = () => {
+      setTimeout(() => {
+        resolve();
+      }, 500);
+    };
+  });
+
+  popup.postMessage({
+    type: 'INIT',
+    message: 'Hello from ' + window.location + ' at ' + new Date()
+  }, uaih);
+
+  alert('Message sent to ' + uaih);
+
+})()
